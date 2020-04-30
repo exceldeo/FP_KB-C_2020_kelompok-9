5f996701b97665c25dd34bc94b12f9c4849e0b39
KresnaAP <Kresnaadhipramana5758@gmail.com>
2020-04-30 22:02:47 +0700

    adding pause menu and end menu

---
+++ CoronaInvasion/CoronaInvasion/Assets/Scripts/EndMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+}
